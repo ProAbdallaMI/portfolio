40e02807e61bff2c73066849a9f718bd5b3dc391
proabdallami <proabdallami@gmail.com>
2025-09-24 10:30:12 +0000

    add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  }
-})
+  base: "/portfolio/",
+	plugins: [react(), tailwindcss()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+});
+
