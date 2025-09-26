20db1d23065330433855f4634bf69c73f7acc9f9
proabdallami <proabdallami@gmail.com>
2025-09-26 14:46:04 +0000

    add responsivenes to menu bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { LuMenu } from "react-icons/lu";
 
 const Navbar = () => {
+  
+  const [navbarStatus, setNavbarStatus] = useState(true);
+  const handleNavbarStatus = () => {
+    setNavbarStatus(!navbarStatus);
+  };
+
   return (
     <nav className="bg-gray-800 p-4 text-white">
-      <div className="container mx-auto flex justify-center items-center">
-        <LuMenu className="absolute top-5 left-5 text-white text-xl" />
+      <div className="container mx-auto flex justify-center items-center md:justify-between">
+        <LuMenu className="absolute top-5 left-5 z-10 text-white text-xl md:hidden" onClick={handleNavbarStatus} />
         <div className="text-lg font-bold">Portfolio</div>
-        <div className="hidden">
+        <div className={`md:flex ${navbarStatus ? "absolute top-0 left-0 w-70/100 h-screen bg-gray-500 flex flex-col" : "hidden"}`}>
           <a href="#home" className="px-3 hover:underline">
             Home
           </a>
