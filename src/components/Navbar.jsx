ebbbed6cc6c0078f1089894a1a1199f15dfb2d62
proabdallami <proabdallami@gmail.com>
2025-09-27 04:14:37 +0000

    formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { LuMenu } from "react-icons/lu";
 
 const Navbar = () => {
-  
   const [navbarStatus, setNavbarStatus] = useState(false);
   const handleNavbarStatus = () => {
     setNavbarStatus(!navbarStatus);
@@ -11,19 +10,40 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white">
       <div className="container mx-auto flex justify-center items-center md:justify-between">
-        <LuMenu className={`absolute top-5 left-5 z-10 text-white text-xl md:hidden transition-transform duration-200 ${navbarStatus && "rotate-180"}`} onClick={handleNavbarStatus} />
+        <LuMenu
+          className={`absolute top-5 left-5 z-10 text-white text-xl md:hidden transition-transform duration-200 ${navbarStatus && "rotate-180"}`}
+          onClick={handleNavbarStatus}
+        />
         <div className="text-lg font-bold">Portfolio</div>
-        <div className={`md:flex transition-transform duration-200 ${navbarStatus ? "absolute top-0 left-0 pt-15 w-70/100 h-screen bg-gray-500 flex flex-col" : "hidden"}`}>
-          <a href="#home" className="px-3 hover:text-red-500" onClick={handleNavbarStatus}>
+        <div
+          className={`md:flex transition-transform duration-200 ${navbarStatus ? "absolute top-0 left-0 pt-15 w-70/100 h-screen bg-gray-500 flex flex-col" : "hidden"}`}
+        >
+          <a
+            href="#home"
+            className="px-3 hover:text-red-500"
+            onClick={handleNavbarStatus}
+          >
             Home
           </a>
-          <a href="#projects" className="px-3 hover:text-red-500" onClick={handleNavbarStatus}>
+          <a
+            href="#projects"
+            className="px-3 hover:text-red-500"
+            onClick={handleNavbarStatus}
+          >
             Projects
           </a>
-          <a href="#skills" className="px-3 hover:text-red-500" onClick={handleNavbarStatus}>
+          <a
+            href="#skills"
+            className="px-3 hover:text-red-500"
+            onClick={handleNavbarStatus}
+          >
             Skills
           </a>
-          <a href="#contact" className="px-3 hover:text-red-500" onClick={handleNavbarStatus}>
+          <a
+            href="#contact"
+            className="px-3 hover:text-red-500"
+            onClick={handleNavbarStatus}
+          >
             Contact
           </a>
         </div>
