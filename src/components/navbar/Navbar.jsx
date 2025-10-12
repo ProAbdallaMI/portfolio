6d8bb01ab28d2608fdc00cf685a4f6549087e3eb
proabdallami <proabdallami@gmail.com>
2025-10-12 00:31:52 +0000

    add motional move to sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import HamburgerToggle from "./HumburgerToggle";
+import Sidebar from "../../sidebar/Sidebar";
 
 const Navbar = () => {
-	const [navbarStatus, setNavbarStatus] = useState(false);
+	const [navbarStatus, setNavbarStatus] = useState(true);
 
 	return (
 		<nav className="bg-white p-4 text-gray-800 fixed w-full top-0 left-0 z-10">
@@ -16,14 +17,14 @@ const Navbar = () => {
 					<a href="#">Portfolio</a>
 				</div>
 
-				<div onClick={() => setNavbarStatus(false)} className={`absolute top-0 left-0 w-full h-screen ${
-							navbarStatus ? "" : "hidden"
+				<div onClick={() => setNavbarStatus(false)} className={`absolute top-0 w-full h-screen -left-full transition-all duration-200 ${
+							navbarStatus ? "translate-x-full" : ""
 						}`}>
 					<div
-						className={`w-70/100 h-screen absolute top-0 left-0 z-10 shadow-2xl bg-white ${
-							navbarStatus ? "" : "hidden"
-						}`}
-					></div>
+						className={`w-70/100 h-screen absolute top-0 left-0 z-10 shadow-2xl bg-white p-10`}
+					>
+						<Sidebar />
+					</div>
 				</div>
 			</div>
 		</nav>
---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+	return (
+    <aside className="flex flex-col justify-center">
+        
+    </aside>
+    );
+};
+
+export default Sidebar;
