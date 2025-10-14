b8cad189143dfea7325b1a538a50403c43ec7b5c
proabdallami <proabdallami@gmail.com>
2025-10-14 10:05:48 +0000

    Add Footer

---
+++ src/sections/Education.jsx
@@ -0,0 +1,7 @@
+const education = () => {
+    return (
+        <section>Education</section>
+    )
+}
+
+export default education;
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <div className="w-full h-10 mt-5 flex justify-center items-center bg-white text-xs">2025 All Rights Reserved. ABDALLA MOHAMED ASHAG</div>
+}
+
+export default Footer;
