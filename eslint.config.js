dd3bb979db82fd53c8470d08f7fa542321e14689
proabdallami <proabdallami@gmail.com>
2025-10-15 13:59:23 +0000

    change eslint to more perfect linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import unusedImports from "eslint-plugin-unused-imports";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
+  reactHooks.configs.flat["recommended-latest"],
+  reactRefresh.configs.vite,
   {
     files: ["**/*.{js,jsx}"],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat["jsx-runtime"],
+    plugins: {
+      "unused-imports": unusedImports,
+    },
     extends: [
       js.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
