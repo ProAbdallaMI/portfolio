5b16778cd31e942cb6738341ef72557f716beb4f
ProAbdallaMI <proabdallami@gmail.com>
2025-10-26 14:08:50 +0400

    add responsiveness to education part

--- src/components/education/Certificate.jsx
+++ src/components/education/Certificate.jsx
@@ -1,12 +1,12 @@
 const Certificate = ({ certData }) => {
 	return (
-		<div className="w-full flex flex-col gap-3">
-			<div className="flex flex-col gap-1">
+		<div className="w-full flex flex-col gap-3 md:flex-row">
+			<div className="flex flex-col gap-1 md:w-40/100 md:gap-5">
 				<h1 className="text-lg font-bold">{certData.school}</h1>
                 <span>Student<span className="text-white text-xs bg-primary px-2 py-0.5 ml-3">{certData.date}</span></span>
 			</div>
-            <div>
-                <h1 className="text-md font-bold">{certData.field}</h1>
+            <div className="md:flex md:flex-col md:gap-5 md:w-60/100 ">
+                <h1 className="text-md font-bold md:text-lg">{certData.field}</h1>
                 <p className="text-paragraph">{certData.about}</p>
             </div>
 		</div>
